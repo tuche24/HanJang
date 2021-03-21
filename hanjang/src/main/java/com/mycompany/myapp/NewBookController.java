cb9799412ca4f1c9fe77f4d19a3926629dddafa1
jeong <tuche24@naver.com>
2021-03-21 21:34:55 +0900

    .

---
+++ hanjang/src/main/java/com/mycompany/myapp/NewBookController.java
@@ -0,0 +1,23 @@
+package com.mycompany.myapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NewBookController {
+
+	@RequestMapping(value = "/goToNewBook.do")
+	public String goToNewBook() {
+		return "new_book/New_Book";
+	}
+	
+	@RequestMapping(value = "/goToNewBookGenre.do")
+	public String goToNewBookGenre() {
+		return "new_book/New_Book_bottomline";
+	}
+
+	@RequestMapping(value = "/goToBookDetail.do")
+	public String goToBookDetail() {
+		return "book_detail/Book_Detail";
+	}
+}
